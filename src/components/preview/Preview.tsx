8b49889bb4670edaa8479e2b1d554a7d00785ec9
Martin Crandon <contact@martincrandon.com>
2026-08-28 13:06:38 +0100

    Dock breadcrumb to preview footer

--- src/components/preview/Preview.tsx
+++ src/components/preview/Preview.tsx
@@ -1534,9 +1534,6 @@ export const Preview = forwardRef<PreviewHandle, PreviewProps>(function Preview(
                   : undefined
               }
             />
-            {activeEditMode && breadcrumbPath.length > 0 && (
-              <PreviewBreadcrumb path={breadcrumbPath} onSelect={selectBreadcrumbItem} />
-            )}
             {/* Structural-edit toolbar, tracking the canvas selection box */}
             {activeEditMode && (
               <ElementToolbar
@@ -1643,6 +1640,9 @@ export const Preview = forwardRef<PreviewHandle, PreviewProps>(function Preview(
             <div className="preview-resize-handle-bar preview-resize-handle-bar--vertical" />
           </div>
         </div>
+        {activeEditMode && breadcrumbPath.length > 0 && (
+          <PreviewBreadcrumb path={breadcrumbPath} onSelect={selectBreadcrumbItem} />
+        )}
       </div>
       {showLogs && (
         <PanelResizeHandle
